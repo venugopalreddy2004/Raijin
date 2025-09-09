93c7b12469850b23747cbce9b730e95f6aa4c624
venugopalreddy2004 <raamigaani2004@gmail.com>
2025-09-10 04:01:08 +0530

    adding api key auth middleware to server

--- backend/primary_server/src/index.ts
+++ backend/primary_server/src/index.ts
@@ -1,5 +1,8 @@
 import express = require("express");
 import redis = require("redis");
+import uuid = require("uuid");
+import crypto = require("crypto");
+import process = require("process")
 
 
 const app = express();
@@ -15,18 +18,47 @@ const client = redis.createClient({
 client.on('error', (err) => console.error('Redis Client Error:', err));
 client.connect();
 
+function auth(req:express.Request, res:express.Response, next:express.NextFunction){
+    const headers = req.headers;
+    const MASTER_API_KEY = process.env.MASTER_API_KEY || "super-secret-key-42";
+    const HASHING_SALT = process.env.HASHING_SALT || 'b1a2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2';
+    const api_key = headers['authorization'] as string;
+    
+    if (!MASTER_API_KEY || !HASHING_SALT) {
+        throw new Error("Missing required environment variables: MASTER_API_KEY or HASHING_SALT");
+    }
+
+    if(api_key===MASTER_API_KEY){
+        // attach userId to req
+        // currently we are using hash of the api key
+        // but IRL we will validate api key with DB and get userId assigned in DB
+        const hmac = crypto.createHmac('sha256',HASHING_SALT);
+        hmac.update(api_key);
+        req.userId = hmac.digest('hex');
+        next();
+    }else{
+        return res.status(403).json({message: "Invalid API Key"})
+    }
+}
+
+app.use(auth);
 
 app.post('/submit',async (req,res)=>{
+    // attaching jobId to req
+    req.jobId = uuid.v4()
+
     try{
         const data = req.body;
-        const userId = data.userId;
-        const jobId = data.jobId;
+        const userId = req.userId;
+        const jobId = req.jobId;
         const s3Address = data.s3Address;
         
-        console.log(userId, jobId, s3Address);
-        res.send("Job submitted to queue!!");
+        console.log(`user id ${userId}`);
+        console.log(`job id ${jobId}`);
+        console.log(`s3 address ${s3Address}`);
         try{
             await client.lPush("workQueue", JSON.stringify({userId, jobId, s3Address}));
+            res.send({taskId: jobId, path : `${userId}/${jobId}.pt`});
             console.log("Successfully added to queue!!");
         }catch(e){
             console.log(e);
